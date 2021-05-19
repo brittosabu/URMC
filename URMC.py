494766f6440fc3b28a6598195ec721419b370652
brittosabu <britto.da3@iiitmk.ac.in>
2021-05-20 00:10:41 +0530

    Added Visualization for feature and weights

--- URMC.py
+++ URMC.py
@@ -1,22 +1,30 @@
 import pandas as pd
 from itertools import combinations
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class URMC:
 
     def __init__(self,features):
         """
+        Initializing weights and features
         """
         self.feature_columns = features
         self.weights = {col: 0 for col in self.feature_columns}
 
-    def calculate_weights(self,data):
+    def fit(self,data):
         """
-        
+        Function to calculate weights using URMC
+        ----------------------------------------
+        Input:
+         data : Data on which weights needs to be calculated
+        Output:
+         None
         """
 
-        for pair in combinations(self.feature_cols,2):
+        for pair in combinations(self.feature_columns,2):
             
-            r = data[pair[0]].r(data[pair[1]])
+            r = data[pair[0]].corr(data[pair[1]])
 
             wi=self.weights[pair[0]]
             wj=self.weights[pair[1]]
@@ -62,13 +70,24 @@ class URMC:
             self.weights[pair[0]]=wi
             self.weights[pair[1]]=wj
 
-        return self.weights
-
-    def calculate_ranks(self,data):
+    def visalize_features(self):
         """
+        Function to Visualize the weights and features
+        """
+        sns.swarmplot(x=list(self.weights.values()),y=list(self.weights.keys()),size=9)
+        plt.show()
+
+    def score(self,data):
+        """
+        Function to score based on trained weights
+        ------------------------------------------
+        Input:
+         data : Data to be score
+        Output:
+         score : Scored data as a series
         """
         data['score'] = 0 
-        for col,weight in self.weights:
+        for col,weight in self.weights.items():
             data['score']+= data[col]*weight
 
         return data['score']
